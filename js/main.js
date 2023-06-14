51f6b57a3355c88d34f5cddc92763c5c4e1a6eb1
martinislas <martin.alejandro.islas@gmail.com>
2023-06-14 15:16:55 +0700

    commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+const toggle = document.getElementById('toggle')
+const menu = document.getElementById('menu')
+
+toggle.addEventListener('click', () => {
+  menu.style.display = 'flex'
+  console.log('test');
+})
